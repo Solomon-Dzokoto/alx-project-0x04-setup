0a5ec9137e5110a4807414329619d90d6545524f
Solomon Elorm Dzokoto <dzokotosolomon85@gmail.com>
2025-06-19 22:48:36 +0000

    Update counter-app.tsx

--- alx-project-0x06/pages/counter-app.tsx
+++ alx-project-0x06/pages/counter-app.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector, useAppDispatch, increment, decrement } from "@/store/store"; // Updated imports
 import { RootState } from "@/store/store"; // Ensure RootState is imported if not already via useAppSelector
-", "
+
+
 const CounterApp: React.FC = () => {
 
   // const count = useAppSelector((state: RootState) => state.counter.value) // Use useAppSelector
